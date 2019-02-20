bb7a6b5ff35da1c5287b87bf90b37efd6c1deae0
bobbyfonken <bobby_fonken@hotmail.com>
2019-02-20 20:47:18 +0000

    Added comments to encrypt.py

--- config/encrypt.py
+++ config/encrypt.py
@@ -1,16 +1,21 @@
+# This script encrypts the config.json file that contains sensitive contents
+
 import gnupg
 import getpass
 
 # Variable to use gnupg
 gpg = gnupg.GPG()
 
+# Get the passphrase to encrypt the file with. This also needs to be given when running the meting.py file
 gpgPass = getpass.getpass("Please provide the passphrase to encrypt the config file: ")
 print("\n")
 
+# Opens the unencrypted file and encrypts the contents --> DELETE THE UNENCRYPTED FILE AFTERWARDS!!!!!
 gpg = gnupg.GPG()
 with open('config.json', 'rb') as f:
 	status = gpg.encrypt_file(f, None, passphrase=gpgPass, symmetric=True, output='config.json.gpg')
 
+# Prints debug information
 print 'ok: ', status.ok
 print 'status: ', status.status
 print 'stderr: ', status.stderr
